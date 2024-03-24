776785c6d19f346a1323a4145649df167cfb8eba
TaimoorRasool2 <taimoor.admission@gmail.com>
2024-03-24 06:44:59 -0600

    CLi Number Guessing game

---
+++ index.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env ts-node
+
+import inquirer from "inquirer";
+var answers= await inquirer.prompt([{
+    name:"guessedNumber",
+    type:"number",
+    message:"please enter number from 1-10"
+}])
+ var randomNunber=Math.floor(Math.random()*10+1);
+
+ if(answers.guessedNumber===randomNunber){
+    console.log("you have Guessed the right number");
+ }else{
+    console.log(`you are wrong and the correctnumber is ${randomNunber}`);
+    
+ }
